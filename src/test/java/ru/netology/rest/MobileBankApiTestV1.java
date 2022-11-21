9143651b6d0e0bce78e166fe6bf1fc544b8b0034
Kirill <sonik3000.83@gmail.com>
2022-11-21 14:29:00 +0300

    initial commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,6 +1,10 @@
 package ru.netology.rest;
 
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -9,19 +13,31 @@ import static io.restassured.RestAssured.given;
 
 
 public class MobileBankApiTestV1 {
+    private final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setBasePath("/api/v1")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
     @Test
     void shouldReturnDemoAccounts() {
-
         given()
-                .baseUri("https://postman-echo.com")
-                .body("some data")
+                .spec(requestSpec)
                 .when()
-                .post("/post")
+                .get("/demo/accounts")
                 .then()
-                .statusCode(200)
-                .body("data", equalTo("some data"));
+                .statusCode(200);
+
+
+
 
     }
+
+
+
 
 
 }
